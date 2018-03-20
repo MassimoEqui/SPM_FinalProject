4e0714f1303951071613c291ae9cd254e0765332
Massimo Equi <massimoequi@ymail.com>
2018-03-20 17:05:57 +0200

    Miron issues.

--- src/main/evolution_cycle.hpp
+++ src/main/evolution_cycle.hpp
@@ -28,7 +28,7 @@ void evolution_cycle(Forest* forest, long tree_no, int depthmax, int threshold,
 	int i = 0;
 	double E = err + 1.0;
 	Tree* bestTree;
-	std::chrono::duration<double> gen_time, overall_time;
+	std::chrono::duration<double> sel_time, mut_cross_time, newg_time, gen_time, overall_time;
 	std::chrono::system_clock::time_point start, end, gen_start, gen_end, overall_start, overall_end;
     overall_start = std::chrono::system_clock::now();
     while(E >= err && ++i<=generation_no){
@@ -38,9 +38,13 @@ void evolution_cycle(Forest* forest, long tree_no, int depthmax, int threshold,
 		int* bestTrees;
 
 		//Selection
+    	start = std::chrono::system_clock::now();
         bestTrees = forest->selectBests(x_vals, y_vals, points_no, threshold);
+    	end = std::chrono::system_clock::now();
+		sel_time = end - start;
 
 		//Mutation&Crossover
+    	start = std::chrono::system_clock::now();
         for(int j=0; j<threshold; j++){
 			int r = std::rand()%3;
 			if(r == 0) newTrees.push_back(forest->getTree(bestTrees[j])->copy());
@@ -54,16 +58,27 @@ void evolution_cycle(Forest* forest, long tree_no, int depthmax, int threshold,
 				newTrees.push_back(forest->crossover(tree1_id, tree2_id));
 			}
 		}
-        forest->newGeneration(newTrees);
+    	end = std::chrono::system_clock::now();
+		mut_cross_time = end - start;
 
+		//Replication
+    	start = std::chrono::system_clock::now();
+        forest->newGeneration(newTrees);
+    	end = std::chrono::system_clock::now();
+		newg_time = end - start;
+
+		gen_end = std::chrono::system_clock::now();
+		gen_time = gen_end - gen_start;
+		
 		if(debug){
 			bestTree = forest->getBestTree(x_vals, y_vals, points_no);
 			E = forest->getBestFitness(x_vals, y_vals, points_no);
-			gen_end = std::chrono::system_clock::now();
-			gen_time = gen_end - gen_start;
 
 			std::cout << "Generation "<<i<<
-			"\n\tTime(s) ---> "<<gen_time.count()<<
+			"\n\tSelection Time(s) = "<<sel_time.count()<<
+			"\n\tMut&Cross Time(s) = "<<mut_cross_time.count()<<
+			"\n\tNew gen   Time(s) = "<<newg_time.count()<<
+			"\n\tWhole gen Time(s) = "<<gen_time.count()<<
 			"\n\tBest Tree = "<< bestTree->toString()<<
 			"\n\tFitness = "<<E<<
             "\n\n";
