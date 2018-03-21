71a1adf32a57402783e2765a1925fae7f4a3ebb6
Massimo Equi <massimoequi@ymail.com>
2018-03-21 03:17:49 +0200

    test.phi.sh edited

--- test.phi.sh
+++ test.phi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 uptime
 
-for ((i=0; i<12; i+=1)); do ./main.out 12000 5 4000 10 20 -1 no < input_cos\(x\)-pow\(x\,3\)10-3.txt ; done
+for ((i=0; i<12; i+=1)); do ./main_mic.out 12000 5 4000 10 20 -1 no < input_cos\(x\)-pow\(x\,3\)10-3.txt ; done
 
-for ((i=0; i<12; i+=1)); do ./parallel_main.out 12000 5 4000 10 20 -1 1 no < input_cos\(x\)-pow\(x\,3\)10-3.txt ; done
+for ((i=0; i<12; i+=1)); do ./parallel_main_mic.out 12000 5 4000 10 20 -1 1 no < input_cos\(x\)-pow\(x\,3\)10-3.txt ; done
 
 for((j=10; j<=120; j+=10)); do
-	for ((i=0; i<12; i+=1)); do ./parallel_main.out 12000 5 4000 10 20 -1 $j no < input_cos\(x\)-pow\(x\,3\)10-3.txt ; done done
+	for ((i=0; i<12; i+=1)); do ./parallel_main_mic.out 12000 5 4000 10 20 -1 $j no < input_cos\(x\)-pow\(x\,3\)10-3.txt ; done done
