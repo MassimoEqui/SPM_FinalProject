03510da83498ff1d4a9fc30be0da95c34400dc5c
Massimo Equi <massimoequi@ymail.com>
2018-03-21 02:24:02 +0200

    edited test.sh

--- src/main/parallel_main.cpp
+++ src/main/parallel_main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char const *argv[])
 		std::cout << "8 parameters needed and an input file to be specified\n"<<
 			"Usage: ./parallel_main.out tree_no depthmax threshold randmax gen_no err nw debug  < input_f(x).txt\n"<<
 			"example\n"<<
-			"./parallel_main.out 12000 5 4000 10 25 0.5 8 no < input_cos(x)-pow(x,3)10-3.txt\n";
+			"./parallel_main.out 12000 5 4000 10 20 0.5 8 no < input_cos(x)-pow(x,3)10-3.txt\n";
 		return 0;
 	}
 
