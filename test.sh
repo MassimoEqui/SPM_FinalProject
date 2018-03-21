eac1ebcc11d39e4493ad23775365eb8b811a65a6
Massimo Equi <massimoequi@ymail.com>
2018-03-21 02:17:58 +0200

    Added test.sh.

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for ((i=0; i<12; i+=1)); do ./main.out 12000 5 4000 10 25 -1 no < input_cos\(x\)-pow\(x\,3\)10-3.txt ; done
+
+for ((i=0; i<12; i+=1)); do ./parallel_main.out 12000 5 4000 10 25 -1 1 no < input_cos\(x\)-pow\(x\,3\)10-3.txt ; done
+
+for((j=2; j<=16; j+=2)); do
+	for ((i=0; i<12; i+=1)); do ./parallel_main.out 12000 5 4000 10 25 -1 $j no < input_cos\(x\)-pow\(x\,3\)10-3.txt ; done done
