12ba8b51b6fb865f581303668f05b9f954cf7a07
Massimo Equi <massimoequi@ymail.com>
2018-03-24 01:39:38 +0200

    Updated test.sh and test.phi.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 uptime
 
+#sequential
 for ((i=0; i<7; i+=1)); do ./main.out 12000 5 4000 10 20 -1 no < input_cos\(x\)-pow\(x\,3\)10-3.txt ; done
 
-
+#fastflow
 for ((i=0; i<7; i+=1)); do ./ff_main.out 12000 5 4000 10 20 -1 1 no < input_cos\(x\)-pow\(x\,3\)10-3.txt ; done
 
 for((j=2; j<=16; j+=2)); do
 	for ((i=0; i<7; i+=1)); do ./ff_main.out 12000 5 4000 10 20 -1 $j no < input_cos\(x\)-pow\(x\,3\)10-3.txt ; done done
 
-
+#threads
 for ((i=0; i<7; i+=1)); do ./thread_main.out 12000 5 4000 10 20 -1 1 no < input_cos\(x\)-pow\(x\,3\)10-3.txt ; done
 
 for((j=2; j<=16; j+=2)); do
